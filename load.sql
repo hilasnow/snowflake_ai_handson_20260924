60bf940de7dfa2d53d2c31ac94c24364755471b2
Yoshikazu Hiraizumi <yoshikazu.hiraizumi@snowflake.com>
2026-09-14 22:07:37 +0900

    API_ALLOWED_PREFIXESの末尾スラッシュを削除(2026側からの同期)

--- load.sql
+++ load.sql
@@ -7,7 +7,7 @@ USE SCHEMA AI_HANDSON_GIT_DB.GIT;
 
 CREATE OR REPLACE API INTEGRATION ai_handson_git_integration
     API_PROVIDER         = git_https_api
-    API_ALLOWED_PREFIXES = ('https://github.com/hilasnow/snowflake_ai_handson_20260924/')
+    API_ALLOWED_PREFIXES = ('https://github.com/hilasnow/snowflake_ai_handson_20260924')
     ENABLED              = TRUE;
 
 CREATE OR REPLACE GIT REPOSITORY ai_handson_git_repo
